be04d4f174190688e0ca87f9d6f6c39d99f22f22
lethal-guitar <lethal_guitar128@web.de>
2016-11-12 16:04:48 +0100

    Introduce GameSessionMode
    A "game session" is a sequence of levels. It starts in-game with the first
    level. Once a level is finished, the bonus-screen is shown, and the next
    level in the sequence is started.
    
    Currently, a game session can only be started with an empty player model,
    starting at the specified level/episode and difficulty. Once save game
    loading is implemented, it will also be possible to create a game session
    with a loaded player model.
    
    Once boss levels are implemented, this mode will also be responsible for
    showing the "episode finished" story screens, as well as the high-score
    entry/display, and going back to the main menu afterwards.

---
+++ src/game_session_mode.hpp
@@ -0,0 +1,56 @@
+/* Copyright (C) 2016, Nikolai Wuttke. All rights reserved.
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#pragma once
+
+#include <base/boost_variant.hpp>
+#include <ui/bonus_screen.hpp>
+
+#include <game_mode.hpp>
+#include <ingame_mode.hpp>
+
+
+namespace rigel {
+
+class GameSessionMode : public GameMode {
+public:
+  GameSessionMode(
+    int episode,
+    int level,
+    data::Difficulty difficulty,
+    Context context);
+
+  void handleEvent(const SDL_Event& event) override;
+  void updateAndRender(engine::TimeDelta dt) override;
+
+private:
+  template<typename StageT>
+  void fadeToNewStage(StageT& stage);
+
+private:
+  using SessionStage = boost::variant<
+    std::unique_ptr<IngameMode>,
+    ui::BonusScreen
+  >;
+
+  SessionStage mCurrentStage;
+  const int mEpisode;
+  int mCurrentLevelNr;
+  const data::Difficulty mDifficulty;
+  Context mContext;
+};
+
+}
