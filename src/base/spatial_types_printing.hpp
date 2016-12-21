86c62db50f1548e4fa5a5a1e3ddfa4d77563ac38
lethal-guitar <lethal_guitar128@web.de>
2016-12-21 15:27:36 +0100

    Add printing stream operator for base::Point

---
+++ src/base/spatial_types_printing.hpp
@@ -0,0 +1,32 @@
+/* Copyright (C) 2016, Nikolai Wuttke. All rights reserved.
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#pragma once
+
+#include "spatial_types.hpp"
+
+#include <iostream>
+
+
+namespace rigel { namespace base {
+
+template<typename ValueT>
+std::ostream& operator<<(std::ostream& stream, const Point<ValueT>& point) {
+  stream << "Point{" << point.x << ", " << point.y << '}';
+  return stream;
+}
+
+}}
