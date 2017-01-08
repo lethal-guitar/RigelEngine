f4cd1f45cee3762fd3f839ef6e7c3194ffb27e71
Nikolai Wuttke <lethal_guitar128@web.de>
2017-01-08 20:16:31 +0100

    Introduce "random" number generator
    This recreates exactly what the original game is doing. The random
    number table is still used directly by the DukeScript runner, since it
    makes the most sense that way for the news reporter animation.

---
+++ src/engine/random_number_generator.hpp
@@ -0,0 +1,36 @@
+/* Copyright (C) 2016, Nikolai Wuttke. All rights reserved.
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#pragma once
+
+#include <array>
+#include <cstdint>
+
+
+namespace rigel { namespace engine {
+
+extern const std::array<int, 256> RANDOM_NUMBER_TABLE;
+
+
+class RandomNumberGenerator {
+public:
+  int gen();
+
+private:
+  std::size_t mNextNumberIndex = 0;
+};
+
+}}
