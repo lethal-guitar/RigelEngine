3a830fd7c38d3e673772f55dff3a3d9345f6eb38
lethal-guitar <lethal_guitar128@web.de>
2017-09-12 18:55:43 +0200

    Lower min required OpenGL version down to 3.0
    This makes the game work on Intel integrated chipsets.

--- src/main.cpp
+++ src/main.cpp
@@ -139,7 +139,7 @@ void initAndRunGame(const string& gamePath, const GameOptions& gameOptions) {
 #else
   SDL_GL_SetAttribute(SDL_GL_CONTEXT_PROFILE_MASK, SDL_GL_CONTEXT_PROFILE_CORE);
   SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 3);
-  SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 2);
+  SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 0);
 #endif
   SDL_GL_SetAttribute(SDL_GL_DOUBLEBUFFER, 1);
 
