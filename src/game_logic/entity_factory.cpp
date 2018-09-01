8564270f527d94db8b864f5a39a2925d434c0bce
Nikolai Wuttke <lethal_guitar128@web.de>
2018-09-01 15:39:01 +0200

    Sort includes

--- src/game_logic/entity_factory.cpp
+++ src/game_logic/entity_factory.cpp
@@ -43,9 +43,9 @@
 #include "game_logic/effect_actor_components.hpp"
 #include "game_logic/effect_components.hpp"
 #include "game_logic/enemy_radar.hpp"
-#include "game_logic/item_container.hpp"
 #include "game_logic/interaction/elevator.hpp"
 #include "game_logic/interaction/force_field.hpp"
+#include "game_logic/item_container.hpp"
 #include "game_logic/trigger_components.hpp"
 
 RIGEL_DISABLE_WARNINGS
