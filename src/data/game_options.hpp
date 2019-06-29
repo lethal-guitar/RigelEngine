1c532c2849268ec8f20c294e54c6bf29cce71476
lethal-guitar <lethal_guitar128@web.de>
2019-06-29 22:14:04 +0200

    Introduce data model for game options

---
+++ src/data/game_options.hpp
@@ -0,0 +1,37 @@
+/* Copyright (C) 2019, Nikolai Wuttke. All rights reserved.
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#pragma once
+
+
+namespace rigel::data {
+
+constexpr auto ENABLE_VSYNC_DEFAULT = true;
+constexpr auto MUSIC_VOLUME_DEFAULT = 1.0f;
+constexpr auto SOUND_VOLUME_DEFAULT = 1.0f;
+
+struct GameOptions {
+  // Graphics
+  bool mEnableVsync = ENABLE_VSYNC_DEFAULT;
+
+  // Sound
+  float mMusicVolume = MUSIC_VOLUME_DEFAULT;
+  float mSoundVolume = SOUND_VOLUME_DEFAULT;
+  bool mMusicOn = true;
+  bool mSoundOn = true;
+};
+
+}
