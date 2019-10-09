49c416f9aad76798a7e77ae74acc59041e074fe2
lethal-guitar <lethal_guitar128@web.de>
2019-10-10 00:13:22 +0200

    Fix JSON merging
    The JSON merge algorithm wasn't correct, as it would cause values in
    the base to become null when they weren't present in the extension.
    
    This change fixes the algorithm to actually do what we want, adds a couple
    of unit tests to ensure correct functionality, and adds some error
    checking and explanatory comments.

--- src/common/json_utils.hpp
+++ src/common/json_utils.hpp
@@ -22,28 +22,49 @@ RIGEL_DISABLE_WARNINGS
 #include <nlohmann/json.hpp>
 RIGEL_RESTORE_WARNINGS
 
+#include <stdexcept>
+
+
 namespace rigel {
 
+constexpr auto CANNOT_MERGE_MESSAGE =
+  "JSON trees are not structurally equivalent";
+
+
 inline nlohmann::json merge(nlohmann::json base, nlohmann::json extension) {
-  if (!extension.is_structured()) {
+  if (!base.is_structured()) {
     return extension;
   }
 
-  if (extension.is_object()) {
+  if (base.is_object()) {
+    if (!extension.is_object()) {
+      throw std::invalid_argument(CANNOT_MERGE_MESSAGE);
+    }
+
+    // First, make sure both base and extension have the same set of properties.
+    // Note that nlohmann::json::insert() does _not_ overwrite properties that
+    // already exist in the target object.
+    base.insert(extension.begin(), extension.end());
     extension.insert(base.begin(), base.end());
 
-    for (auto& [key, value] : extension.items()) {
-      value = merge(value, base[key]);
+    // Now recursively merge all properties. This will overwrite any primitive
+    // values in base with their extension counterparts.
+    for (auto& [key, value] : base.items()) {
+      value = merge(value, extension[key]);
     }
   } else { // array
+    if (!extension.is_array() || base.size() != extension.size()) {
+      throw std::invalid_argument(CANNOT_MERGE_MESSAGE);
+    }
+
     auto index = 0u;
-    for (auto& value : extension) {
-      value = merge(value, base[index]);
+    for (auto& value : base) {
+      value = merge(value, extension[index]);
       ++index;
     }
   }
 
-  return extension;
+  return base;
 }
 
 }
