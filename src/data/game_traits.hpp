7b18391d7d7e0b808205666789dac454435bae60
Nikolai Wuttke <lethal_guitar128@web.de>
2019-12-10 14:56:11 +0100

    Make game traits constants constexpr

--- src/data/game_traits.hpp
+++ src/data/game_traits.hpp
@@ -31,33 +31,37 @@ enum class TileImageType {
 
 struct GameTraits {
 
-  static const int tileSize = 8;
-  static const int tileSizeSquared = tileSize*tileSize;
+  static constexpr int tileSize = 8;
+  static constexpr int tileSizeSquared = tileSize*tileSize;
 
-  static const int viewPortWidthPx = 320;
-  static const int viewPortHeightPx = 200;
-  static const int viewPortWidthTiles =
+  static constexpr int viewPortWidthPx = 320;
+  static constexpr int viewPortHeightPx = 200;
+  static constexpr int viewPortWidthTiles =
     viewPortWidthPx / tileSize;
-  static const int viewPortHeightTiles =
+  static constexpr int viewPortHeightTiles =
     viewPortHeightPx / tileSize;
 
   // The actual in-game viewport starts with an offset and is further reduced
   // to make room for the HUD. The right hand side features another 8px of
   // black border.
-  static const base::Vector inGameViewPortOffset;
-  static const base::Extents inGameViewPortSize;
+  static constexpr base::Vector inGameViewPortOffset{8, 8};
+  static constexpr base::Extents inGameViewPortSize{
+    GameTraits::viewPortWidthPx - 16,
+    GameTraits::viewPortHeightPx - 8};
 
-  static const int mapViewPortWidthTiles =
+  static constexpr int mapViewPortWidthTiles =
     viewPortWidthTiles - 8;
-  static const int mapViewPortHeightTiles =
+  static constexpr int mapViewPortHeightTiles =
     viewPortHeightTiles - 5;
 
-  static const base::Extents mapViewPortSize;
+  static constexpr base::Extents mapViewPortSize{
+    GameTraits::mapViewPortWidthTiles,
+    GameTraits::mapViewPortHeightTiles};
 
-  static const std::size_t egaPlanes = 4u;
-  static const std::size_t maskedEgaPlanes = egaPlanes + 1u;
-  static const std::size_t fontEgaPlanes = 2u;
-  static const std::size_t pixelsPerEgaByte = 8u;
+  static constexpr std::size_t egaPlanes = 4u;
+  static constexpr std::size_t maskedEgaPlanes = egaPlanes + 1u;
+  static constexpr std::size_t fontEgaPlanes = 2u;
+  static constexpr std::size_t pixelsPerEgaByte = 8u;
 
   static constexpr std::size_t numPlanes(const TileImageType type) {
     return type == TileImageType::Masked ? maskedEgaPlanes : egaPlanes;
@@ -71,16 +75,16 @@ struct GameTraits {
     return (tileSizeSquared / pixelsPerEgaByte) * fontEgaPlanes;
   }
 
-  static const base::Extents menuFontCharacterBitmapSizeTiles;
+  static constexpr base::Extents menuFontCharacterBitmapSizeTiles{1, 2};
 
-  static const std::size_t mapDataWords = 32750u;
+  static constexpr std::size_t mapDataWords = 32750u;
 
   static constexpr std::size_t mapHeightForWidth(const std::size_t width) {
     return mapDataWords / width;
   }
 
-  static const int minDrawOrder = -1;
-  static const int maxDrawOrder = 4;
+  static constexpr int minDrawOrder = -1;
+  static constexpr int maxDrawOrder = 4;
 
   // The game's original 320x200 resolution would give us a 16:10 aspect ratio
   // when using square pixels, but monitors of the time had a 4:3 aspect ratio,
@@ -91,27 +95,27 @@ struct GameTraits {
   static constexpr auto aspectRatio = 4.0f / 3.0f;
 
   struct CZone {
-    static const std::size_t numSolidTiles = 1000u;
-    static const std::size_t numMaskedTiles = 160u;
-    static const std::size_t numTilesTotal = numSolidTiles + numMaskedTiles;
+    static constexpr std::size_t numSolidTiles = 1000u;
+    static constexpr std::size_t numMaskedTiles = 160u;
+    static constexpr std::size_t numTilesTotal = numSolidTiles + numMaskedTiles;
 
-    static const int tileSetImageWidth = viewPortWidthTiles;
-    static const int solidTilesImageHeight = viewPortHeightTiles;
-    static const int tileSetImageHeight =
+    static constexpr int tileSetImageWidth = viewPortWidthTiles;
+    static constexpr int solidTilesImageHeight = viewPortHeightTiles;
+    static constexpr int tileSetImageHeight =
       solidTilesImageHeight + numMaskedTiles/tileSetImageWidth;
 
-    static const std::size_t tileBytes =
+    static constexpr std::size_t tileBytes =
       egaPlanes * tileSize * sizeof(std::uint8_t);
-    static const std::size_t tileBytesMasked =
+    static constexpr std::size_t tileBytesMasked =
       maskedEgaPlanes * tileSize * sizeof(std::uint8_t);
 
-    static const std::size_t attributeBytes = sizeof(std::uint16_t);
+    static constexpr std::size_t attributeBytes = sizeof(std::uint16_t);
 
-    static const std::size_t attributeBytesTotal =
+    static constexpr std::size_t attributeBytesTotal =
       attributeBytes*numSolidTiles + attributeBytes*5*numMaskedTiles;
   };
 
-  static const auto musicPlaybackRate = 280;
+  static constexpr auto musicPlaybackRate = 280;
 };
 
 
