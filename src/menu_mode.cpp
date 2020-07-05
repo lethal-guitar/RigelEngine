lethal-guitar/RigelEngine

commit 67354a453565114e17d49f8f7be430f49d938571

Unify menu input event handling

src/menu_mode.cpp
@@ -23,6 +23,7 @@
 #include "data/game_session_data.hpp"
 #include "loader/resource_loader.hpp"
 #include "ui/high_score_list.hpp"
+#include "ui/menu_navigation.hpp"
 
 
 namespace rigel {
@@ -60,23 +61,12 @@ MenuMode::MenuMode(Context context)
 
 
 void MenuMode::handleEvent(const SDL_Event& event) {
-  if (mOptionsMenu) {
-    if (
-      (event.type == SDL_KEYDOWN && event.key.keysym.sym == SDLK_ESCAPE) ||
-      (event.type == SDL_CONTROLLERBUTTONDOWN &&
-       event.cbutton.button == SDL_CONTROLLER_BUTTON_B)
-    ) {
-      mOptionsMenu = std::nullopt;
-    }
+  if (mOptionsMenu && ui::isCancelButton(event)) {
+    mOptionsMenu = std::nullopt;
     return;
   }
 
-  if (
-    mMenuState == MenuState::AskIfQuit &&
-    ((event.type == SDL_KEYDOWN && event.key.keysym.sym == SDLK_y) ||
-     (event.type == SDL_CONTROLLERBUTTONDOWN &&
-      event.cbutton.button == SDL_CONTROLLER_BUTTON_A))
-  ) {
+  if (mMenuState == MenuState::AskIfQuit && ui::isQuitConfirmButton(event)) {
     mContext.mpServiceProvider->scheduleGameQuit();
     return;
   }
@@ -84,16 +74,7 @@ void MenuMode::handleEvent(const SDL_Event& event) {
   if (mMenuState == MenuState::MainMenu) {
     const auto maybeIndex = mContext.mpScriptRunner->currentPageIndex();
     const auto optionsMenuSelected = maybeIndex && *maybeIndex == 2;
-    if (
-      optionsMenuSelected &&
-      ((event.type == SDL_KEYDOWN && (
-        event.key.keysym.sym == SDLK_RETURN ||
-        event.key.keysym.sym == SDLK_KP_ENTER ||
-        event.key.keysym.sym == SDLK_SPACE))
-      ||
-      (event.type == SDL_CONTROLLERBUTTONDOWN &&
-       event.cbutton.button == SDL_CONTROLLER_BUTTON_A))
-    ) {
+    if (optionsMenuSelected && ui::isConfirmButton(event)) {
       mOptionsMenu = ui::OptionsMenu{
         mContext.mpUserProfile,
         mContext.mpServiceProvider,

src/anti_piracy_screen_mode.cpp
@@ -20,6 +20,8 @@
 
 #include "intro_demo_loop_mode.hpp"
 
+#include "ui/menu_navigation.hpp"
+
 
 namespace rigel {
 
@@ -42,7 +44,7 @@ std::unique_ptr<GameMode> AntiPiracyScreenMode::updateAndRender(
 
   const auto anyButtonPressed = any_of(begin(events), end(events),
     [](const SDL_Event& event) {
-      return (event.type == SDL_KEYDOWN) || (event.type == SDL_CONTROLLERBUTTONDOWN);
+      return ui::isButtonPress(event);
     });
 
   if (anyButtonPressed) {

src/game_session_mode.cpp
@@ -24,6 +24,7 @@
 #include "common/user_profile.hpp"
 #include "data/saved_game.hpp"
 #include "ui/high_score_list.hpp"
+#include "ui/menu_navigation.hpp"
 
 
 namespace rigel {
@@ -80,22 +81,14 @@ void GameSessionMode::handleEvent(const SDL_Event& event) {
         mContext.mpServiceProvider->fadeOutScreen();
       };
 
-      if (event.type == SDL_KEYDOWN && event.key.repeat == 0) {
-        switch (event.key.keysym.sym) {
-          case SDLK_ESCAPE:
-            fadeOut();
-            enterHighScore("");
-            return;
-
-          case SDLK_KP_ENTER:
-          case SDLK_RETURN:
-            fadeOut();
-            enterHighScore(state.mNameEntryWidget.text());
-            return;
-
-          default:
-            break;
-        }
+      if (ui::isCancelButton(event)) {
+        fadeOut();
+        enterHighScore("");
+        return;
+      } else if (ui::isConfirmButton(event)) {
+        fadeOut();
+        enterHighScore(state.mNameEntryWidget.text());
+        return;
       }
 
       state.mNameEntryWidget.handleEvent(event);

src/intro_demo_loop_mode.cpp
@@ -5,6 +5,7 @@
 #include "common/game_service_provider.hpp"
 #include "loader/resource_loader.hpp"
 #include "ui/duke_script_runner.hpp"
+#include "ui/menu_navigation.hpp"
 
 
 namespace rigel {
@@ -93,7 +94,7 @@ IntroDemoLoopMode::IntroDemoLoopMode(
 
 
 bool IntroDemoLoopMode::handleEvent(const SDL_Event& event) {
-  if ((event.type != SDL_KEYDOWN) && (event.type != SDL_CONTROLLERBUTTONDOWN)) {
+  if (!ui::isButtonPress(event)) {
     return false;
   }
 
@@ -109,12 +110,7 @@ bool IntroDemoLoopMode::handleEvent(const SDL_Event& event) {
   } else {
     auto& currentStage = mStages[mCurrentStage];
 
-    if (
-      (event.type == SDL_KEYDOWN && event.key.keysym.sym == SDLK_ESCAPE) ||
-      (event.type == SDL_CONTROLLERBUTTONDOWN &&
-       event.cbutton.button == SDL_CONTROLLER_BUTTON_B) ||
-      !canStageHandleEvents(currentStage)
-    ) {
+    if (ui::isCancelButton(event) && !canStageHandleEvents(currentStage)) {
       return true;
     } else {
       forwardEventToStage(currentStage, event);

src/ui/episode_end_sequence.cpp
@@ -19,6 +19,7 @@
 #include "base/container_utils.hpp"
 #include "common/game_service_provider.hpp"
 #include "engine/timing.hpp"
+#include "ui/menu_navigation.hpp"
 #include "ui/utils.hpp"
 
 
@@ -86,7 +87,7 @@ void EpisodeEndScreen::updateAndRender(engine::TimeDelta dt) {
 
 
 void EpisodeEndScreen::handleEvent(const SDL_Event& event) {
-  if ((event.type != SDL_KEYDOWN) && (event.type != SDL_CONTROLLERBUTTONDOWN)) {
+  if (!ui::isButtonPress(event)) {
     return;
   }
 

src/ui/menu_navigation.cpp
@@ -31,14 +31,20 @@ bool isNonRepeatKeyDown(const SDL_Event& event) {
 }
 
 
+bool isButtonPress(const SDL_Event& event) {
+  return event.type == SDL_KEYDOWN || event.type == SDL_CONTROLLERBUTTONDOWN;
+}
+
+
 bool isConfirmButton(const SDL_Event& event) {
-  const auto enterPressed = isNonRepeatKeyDown(event) &&
+  const auto enterOrSpacePressed = isNonRepeatKeyDown(event) &&
     (event.key.keysym.sym == SDLK_RETURN ||
-     event.key.keysym.sym == SDLK_KP_ENTER);
+     event.key.keysym.sym == SDLK_KP_ENTER ||
+     event.key.keysym.sym == SDLK_SPACE);
   const auto buttonAPressed = event.type == SDL_CONTROLLERBUTTONDOWN &&
     event.cbutton.button == SDL_CONTROLLER_BUTTON_A;
 
-  return enterPressed || buttonAPressed;
+  return enterOrSpacePressed || buttonAPressed;
 }
 
 
@@ -49,6 +55,16 @@ bool isCancelButton(const SDL_Event& event) {
     event.cbutton.button == SDL_CONTROLLER_BUTTON_B;
 
   return escapePressed || buttonBPressed;
+}
+
+
+bool isQuitConfirmButton(const SDL_Event& event) {
+  const auto yPressed =
+    event.type == SDL_KEYDOWN && event.key.keysym.sym == SDLK_y;
+  const auto buttonAPressed = event.type == SDL_CONTROLLERBUTTONDOWN &&
+    event.cbutton.button == SDL_CONTROLLER_BUTTON_A;
+
+  return yPressed || buttonAPressed;
 }
 
 

src/ui/menu_navigation.hpp
@@ -27,8 +27,10 @@ RIGEL_RESTORE_WARNINGS
 namespace rigel::ui {
 
 bool isNonRepeatKeyDown(const SDL_Event& event);
+bool isButtonPress(const SDL_Event& event);
 bool isConfirmButton(const SDL_Event& event);
 bool isCancelButton(const SDL_Event& event);
+bool isQuitConfirmButton(const SDL_Event& event);
 
 
 enum class NavigationEvent {

commit 4e1458c1a9f619a4fb4834ff24cf5692cedf4c12

Fix main menu still receiving input when options menu active
This broke in 67354a45356.

src/menu_mode.cpp
@@ -66,6 +66,11 @@ void MenuMode::handleEvent(const SDL_Event& event) {
     return;
   }
 
+  if (mOptionsMenu) {
+    // Options menu blocks all input
+    return;
+  }
+
   if (mMenuState == MenuState::AskIfQuit && ui::isQuitConfirmButton(event)) {
     mContext.mpServiceProvider->scheduleGameQuit();
     return;