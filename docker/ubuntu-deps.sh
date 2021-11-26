dd3422f4cba5a4a7ba5145cfe94285fa6882b914
lethal-guitar <lethal_guitar128@web.de>
2021-11-26 14:40:19 +0100

    Clean up CI config

--- docker/ubuntu-deps.sh
+++ docker/ubuntu-deps.sh
@@ -6,8 +6,6 @@ apt-get update && \
 DEBIAN_FRONTEND="noninteractive" apt-get install --no-install-recommends -y \
     cmake \
     g++-8 \
-    libboost-dev \
-    libboost-program-options-dev \
     libsdl2-dev \
     libsdl2-mixer-dev \
     make
