e370a16da4061d785d71389fe086224708c61cb5
Nikolai Wuttke <lethal_guitar128@web.de>
2021-12-04 13:24:35 +0100

    Integrate Nuked OPL3 (not selectable yet)

--- src/loader/adlib_emulator.hpp
+++ src/loader/adlib_emulator.hpp
@@ -19,27 +19,30 @@
 #include "base/math_tools.hpp"
 
 #include <dbopl.h>
+#include <opl3.h>
 
 #include <algorithm>
 #include <array>
 #include <cstdint>
+#include <memory>
+#include <variant>
 
 
 namespace rigel::loader
 {
 
-class AdlibEmulator
+namespace detail
+{
+
+class DbOplAdlibEmulator
 {
 public:
-  explicit AdlibEmulator(int sampleRate)
+  explicit DbOplAdlibEmulator(int sampleRate)
     : mEmulator(sampleRate)
   {
-    // This is normally done by the game to select the right type of wave forms.
-    // It's not part of the IMF files.
-    mEmulator.WriteReg(1, 32);
   }
 
-  void writeRegister(const std::uint32_t reg, const std::uint8_t value)
+  void writeRegister(const std::uint8_t reg, const std::uint8_t value)
   {
     mEmulator.WriteReg(reg, value);
   }
@@ -77,6 +80,109 @@ public:
 private:
   DBOPL::Chip mEmulator;
   std::array<std::int32_t, 256> mTempBuffer;
+};
+
+
+class NukedOpl3AdlibEmulator
+{
+public:
+  explicit NukedOpl3AdlibEmulator(int sampleRate)
+  {
+    OPL3_Reset(&mEmulator, sampleRate);
+  }
+
+  NukedOpl3AdlibEmulator(const NukedOpl3AdlibEmulator&) = delete;
+  NukedOpl3AdlibEmulator& operator=(const NukedOpl3AdlibEmulator&) = delete;
+
+  void writeRegister(const std::uint8_t reg, const std::uint8_t value)
+  {
+    OPL3_WriteRegBuffered(&mEmulator, reg, value);
+  }
+
+  template <typename OutputIt>
+  void render(
+    std::size_t numSamples,
+    OutputIt destination,
+    const float volumeScale = 1.0f)
+  {
+    std::array<std::int16_t, 2> stereoPair;
+
+    for (auto i = 0u; i < numSamples; ++i)
+    {
+      OPL3_GenerateResampled(&mEmulator, stereoPair.data());
+      const auto mixedSample = stereoPair[0] * 0.5f + stereoPair[1] * 0.5f;
+      *destination++ = base::roundTo<int16_t>(mixedSample * volumeScale);
+    }
+  }
+
+private:
+  opl3_chip mEmulator;
+};
+
+} // namespace detail
+
+
+class AdlibEmulator
+{
+public:
+  enum class Type
+  {
+    DBOPL,
+    NukedOpl3
+  };
+
+  explicit AdlibEmulator(const int sampleRate, const Type type = Type::DBOPL)
+    : mpEmulator([&]() {
+      if (type == Type::NukedOpl3)
+      {
+        return std::make_unique<Emulator>(
+          std::in_place_type<detail::NukedOpl3AdlibEmulator>, sampleRate);
+      }
+
+      return std::make_unique<Emulator>(
+        std::in_place_type<detail::DbOplAdlibEmulator>, sampleRate);
+    }())
+  {
+    // This is normally done by the game to select the right type of wave forms.
+    // It's not part of the IMF files.
+    writeRegister(1, 32);
+  }
+
+  void writeRegister(const std::uint8_t reg, const std::uint8_t value)
+  {
+    std::visit(
+      [reg, value](auto&& emulator) { emulator.writeRegister(reg, value); },
+      *mpEmulator);
+  }
+
+  template <typename OutputIt>
+  void render(
+    std::size_t numSamples,
+    OutputIt destination,
+    const float volumeScale = 1.0f)
+  {
+    std::visit(
+      [&](auto&& emulator) {
+        emulator.render(numSamples, destination, volumeScale);
+      },
+      *mpEmulator);
+  }
+
+  Type type() const
+  {
+    if (std::holds_alternative<detail::NukedOpl3AdlibEmulator>(*mpEmulator))
+    {
+      return Type::NukedOpl3;
+    }
+
+    return Type::DBOPL;
+  }
+
+private:
+  using Emulator =
+    std::variant<detail::DbOplAdlibEmulator, detail::NukedOpl3AdlibEmulator>;
+
+  std::unique_ptr<Emulator> mpEmulator;
 };
 
 } // namespace rigel::loader
