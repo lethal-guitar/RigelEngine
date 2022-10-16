10efe676fa68fcde389130cc365ebf14b3f10ea8
lethal-guitar <lethal_guitar128@web.de>
2022-10-16 12:52:23 +0200

    Unify license headers
    The clause about Cosmore doesn't apply to any of these files.

--- src/game_logic_classic/sprite.c
+++ src/game_logic_classic/sprite.c
@@ -1,26 +1,17 @@
 /* Copyright (C) 2022, Nikolai Wuttke. All rights reserved.
  *
- * This project is based on disassembly of NUKEM2.EXE from the game
- * Duke Nukem II, Copyright (C) 1993 Apogee Software, Ltd.
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 2 of the License, or
+ *  (at your option) any later version.
  *
- * Some parts of the code are based on or have been adapted from the Cosmore
- * project, Copyright (c) 2020-2022 Scott Smitelli.
- * See LICENSE_Cosmore file at the root of the repository, or refer to
- * https://github.com/smitelli/cosmore/blob/master/LICENSE.
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
  *
- *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 2 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
 #include "actors.h"
