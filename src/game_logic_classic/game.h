ad747bed7add31f643540ec7985e3fccbed54a6e
lethal-guitar <lethal_guitar128@web.de>
2022-10-22 15:10:55 +0200

    Merge vars.h into game.h

--- src/game_logic_classic/game.h
+++ src/game_logic_classic/game.h
@@ -471,3 +471,123 @@ typedef struct
 
 // Convert a tile value to pixels (multiply by 8)
 #define T2PX(val) (val << 3)
+
+
+//
+// Global game state
+//
+
+typedef struct Context_
+{
+  bool sysTecMode;
+  byte retConveyorBeltCheckResult;
+  byte mapViewportHeight;
+  bool gfxFlashScreen;
+  byte gfxScreenFlashColor;
+  bool gmIsTeleporting;
+  int16_t plCollectedLetters;
+  word gmTeleportTargetPosX;
+  word gmTeleportTargetPosY;
+  byte retPlayerShotDirection;
+  bool gmPlayerTookDamage;
+  word far* gfxTilesetAttributes;
+  word mapBottom;
+  word mapWidthShift;
+  word mapWidth;
+  word gmCameraPosX;
+  word gmCameraPosY;
+  word far* mapData;
+  word far gmTileDebrisStates[700 * 5];
+  word far* gfxActorInfoData;
+  word gmNumActors;
+  bool gmBossActivated;
+  bool plRapidFireIsActiveFrame;
+  bool gmRequestUnlockNextDoor;
+  byte gmCurrentEpisode;
+  byte gmCurrentLevel;
+  ActorState gmActorStates[MAX_NUM_ACTORS];
+  word levelActorListSize;
+  word gfxCurrentDisplayPage;
+  byte gmGameState;
+  EffectState gmEffectStates[MAX_NUM_EFFECTS];
+  PlayerShot gmPlayerShotStates[MAX_NUM_PLAYER_SHOTS];
+  bool inputMoveUp;
+  bool inputMoveDown;
+  bool inputMoveLeft;
+  bool inputMoveRight;
+  bool inputJump;
+  bool inputFire;
+  word plRapidFireTimeLeft;
+  dword plScore;
+  bool mapParallaxHorizontal;
+  bool mapHasReactorDestructionEvent;
+  bool mapSwitchBackdropOnTeleport;
+  byte gmRngIndex;
+  bool plOnElevator;
+  byte plAirlockDeathStep;
+  byte plBodyExplosionStep;
+  byte plFallingSpeed;
+  byte plDeathAnimationStep;
+  byte plState;
+  byte plJumpStep;
+  byte plMercyFramesLeft;
+  word plPosX;
+  word plPosY;
+  word gmBeaconPosX;
+  word gmBeaconPosY;
+  byte plActorId;
+  byte plAnimationFrame;
+  bool plKilledInShip;
+  word gmPlayerEatingActor;
+  bool gmRequestUnlockNextForceField;
+  byte plInteractAnimTicks;
+  bool plBlockLookingUp;
+  bool mapHasEarthquake;
+  byte gmEarthquakeCountdown;
+  byte gmEarthquakeThreshold;
+  byte gmReactorDestructionStep;
+  byte gmNumMovingMapParts;
+  word plCloakTimeLeft;
+  MovingMapPartState gmMovingMapParts[MAX_NUM_MOVING_MAP_PARTS];
+  word gmCamerasDestroyed;
+  word gmCamerasInLevel;
+  word gmWeaponsCollected;
+  word gmWeaponsInLevel;
+  word gmMerchCollected;
+  word gmMerchInLevel;
+  word gmTurretsDestroyed;
+  word gmTurretsInLevel;
+  word gmOrbsLeft;
+  word gmBombBoxesLeft;
+  word plAttachedSpider1;
+  word plAttachedSpider2;
+  word plAttachedSpider3;
+  word gmBossHealth;
+  byte gmRadarDishesLeft;
+  word gmCloakPickupPosX;
+  word gmCloakPickupPosY;
+  word gmExplodingSectionLeft;
+  word gmExplodingSectionTop;
+  word gmExplodingSectionRight;
+  word gmExplodingSectionBottom;
+  byte gmExplodingSectionTicksElapsed;
+  word gmActiveFanIndex;
+  bool plBlockJumping;
+  bool plWalkAnimTicksDue;
+  bool plBlockShooting;
+  byte levelHeaderData[3002];
+  word mmChunkSizes[MM_MAX_NUM_CHUNKS];
+  ChunkType mmChunkTypes[MM_MAX_NUM_CHUNKS];
+  dword mmMemTotal;
+  dword mmMemUsed;
+  word mmChunksUsed;
+  word far* psParticleData[NUM_PARTICLE_GROUPS];
+  ParticleGroup psParticleGroups[NUM_PARTICLE_GROUPS];
+  bool bdUseSecondary;
+  byte gmDifficulty;
+  byte plWeapon;
+  byte plAmmo;
+  byte plHealth;
+  bool gmBeaconActivated;
+  byte mmRawMem[MM_TOTAL_SIZE];
+} Context;
