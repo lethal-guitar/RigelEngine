10c29e62766fb4873a06025e84540eaa09412316
lethal-guitar <lethal_guitar128@web.de>
2022-10-22 14:43:16 +0200

    Remove unnecessary declarations
    We will re-add the function declarations later.

--- src/game_logic_classic/game.h
+++ src/game_logic_classic/game.h
@@ -86,13 +86,6 @@ typedef enum
 } PaletteColor;
 
 
-#ifndef SHAREWARE
-  #define REGISTERED_VERSION
-#endif
-
-#define RADAR_POS_X 288
-#define RADAR_POS_Y 136
-
 #define ORIENTATION_LEFT 0
 #define ORIENTATION_RIGHT 1
 
@@ -394,10 +387,6 @@ typedef struct
     }                                                                          \
   }
 
-#define LVL_TILESET_FILENAME() (levelHeaderData)
-#define LVL_BACKDROP_FILENAME() (levelHeaderData + 13)
-#define LVL_MUSIC_FILENAME() (levelHeaderData + 26)
-
 #define READ_LVL_HEADER_WORD(offset)                                           \
   ((*(levelHeaderData + offset + 1) << 8) | *(levelHeaderData + offset))
 
@@ -451,32 +440,9 @@ typedef enum
 } ChunkType;
 
 
-typedef enum
-{
-  VT_APOGEE_LOGO = 8,
-  VT_NEO_LA = 0,
-  VT_RANGE_1 = 4,
-  VT_RANGE_2 = 5,
-  VT_RANGE_3 = 6,
-  VT_UNUSED_1 = 2,
-  VT_UNUSED_2 = 3,
-} VideoType;
-
-
-#define XY_TO_OFFSET(x, y) (x * 8 + y * 320)
-
 // Convert a tile value to pixels (multiply by 8)
 #define T2PX(val) (val << 3)
 
-#define ANY_KEY_PRESSED() !(kbLastScancode & 0x80)
-#define LAST_SCANCODE() (kbLastScancode & 0x7F)
-
-#define CLEAR_SCREEN()                                                         \
-  FillScreenRegion(                                                            \
-    SFC_BLACK, 0, 0, SCREEN_WIDTH_TILES - 1, SCREEN_HEIGHT_TILES - 1);
-
-
-typedef char SaveSlotName[SAVE_SLOT_NAME_MAX_LEN + 1];
 
 typedef struct
 {
@@ -485,165 +451,3 @@ typedef struct
   word y;
   word color;
 } ParticleGroup;
-
-
-// Functions from unit1.c
-byte pascal DN2_inportb(int16_t address);
-void pascal DN2_outportb(int16_t address, byte value);
-void pascal DN2_outport(int16_t address, word value);
-
-void pascal CopyStringUppercased(const char far* src, char far* dest);
-bool pascal StringStartsWith(const char far* string, const char far* prefix);
-word pascal DN2_strlen(char far* string);
-
-byte RandomNumber(void);
-
-bool MM_Init(void);
-dword MM_MemAvailable(void);
-void far* MM_PushChunk(word size, ChunkType type);
-void pascal MM_PopChunk(ChunkType type);
-void pascal MM_PopChunks(ChunkType type);
-
-int16_t pascal OpenFileRW(char* name);
-int16_t pascal OpenFileW(char* name);
-void pascal WriteWord(word value, int16_t fd);
-word pascal ReadWord(int16_t fd);
-void pascal CloseFile(int16_t fd);
-char far* MakeFilename(char far* prefix, byte number, char far* postfix);
-
-void LoadGroupFileDict(void);
-dword pascal OpenAssetFile(const char far* name, int16_t* pOutFd);
-word pascal GetAssetFileSize(const char far* name);
-void pascal LoadAssetFile(const char far* name, void far* buffer);
-void pascal LoadAssetFilePart(
-  const char far* name,
-  dword offset,
-  void far* buffer,
-  word size);
-
-void pascal UploadTileset(byte far* data, word size, word targetOffset);
-
-void FadeOutScreen(void);
-void Duke3dTeaserFadeIn(byte step);
-void FadeInScreen(void);
-
-void pascal InitParticleSystem(void);
-void pascal
-  SpawnParticles(word x, word y, signed char xVelocityScale, byte color);
-void pascal UpdateAndDrawParticles(void);
-void pascal ClearParticles(void);
-
-void InstallTimerInterrupt(void);
-void RestoreTimerInterrupt(void);
-
-void pascal WaitTicks(word ticks);
-
-void pascal PlayMusic(char far* filename, void far* buffer);
-void pascal StartMusicPlayback(int16_t far* data);
-void StopMusic(void);
-void ResetAdLibMusicChannels(void);
-
-void pascal PollJoystick(void);
-void interrupt KeyboardHandler(void);
-
-byte pascal AwaitInput(void);
-byte AwaitInputOrTimeout(word ticksToWait);
-byte pascal GetTextInput(word x, word y);
-void pascal ToggleCheckbox(byte index, byte* checkboxData);
-
-word pascal FindNextToken(char far* str);
-word pascal FindTokenForwards(char far* token, char far* str);
-word pascal FindTokenBackwards(char far* token, char far* str);
-char pascal TerminateStrAfterToken(char far* str);
-char pascal TerminateStrAtEOL(char far* str);
-void pascal SetUpParameterRead(char far** pText, char* outOriginalEnd);
-void pascal UnterminateStr(char far* str, char newEnd);
-
-void pascal DrawStatusIcon_1x1(word srcOffset, word x, word y);
-void pascal DrawStatusIcon_1x2(word srcOffset, word x, word y);
-void pascal DrawStatusIcon_2x1(word srcOffset, word x, word y);
-void pascal DrawStatusIcon_2x2(word srcOffset, word x, word y);
-
-void pascal FillScreenRegion(
-  word fillTileIndex,
-  word left,
-  word bottom,
-  word right,
-  word top);
-
-void SetScreenShift(byte amount);
-
-void pascal DrawText(word x, word y, char far* text);
-void pascal DrawFullscreenImage(char far* filename);
-
-void pascal DrawSaveSlotNames(word selectedIndex);
-void pascal DrawKeyBindings(void);
-void pascal DrawCheckboxes(byte x, byte* checkboxData);
-void pascal UnfoldMessageBoxFrame(int16_t top, int16_t height, int16_t width);
-void ShowBonusScreen(void);
-bool pascal RunSaveGameNameEntry(word index);
-void pascal DrawHighScoreList(byte episode);
-void pascal RunRebindKeyDialog(byte index);
-bool pascal RunJoystickCalibration(void);
-void pascal TryAddHighScore(byte episode);
-
-// This function seems to have been redeclared in unit2.c, with a
-// mismatching return type. The function itself returns a word-sized
-// value, but calling code treats it as if returning a byte-sized one.
-// So we don't declare it here, and instead declare it (with the wrong
-// return type) at the top of unit2.c
-//
-// ibool pascal IsSaveSlotEmpty(byte index);
-
-void pascal HUD_DrawLevelNumber(word level);
-void pascal HUD_DrawBackground(void);
-void pascal HUD_DrawHealth(word health);
-void pascal HUD_DrawLowHealthAnimation(word health);
-void pascal HUD_DrawAmmo(word ammo);
-void pascal HUD_DrawWeapon(int16_t weapon);
-void pascal HUD_DrawInventory(void);
-void pascal GiveScore(word score);
-void pascal AddInventoryItem(word item);
-bool pascal RemoveFromInventory(word item);
-void pascal ClearInventory(void);
-void pascal HUD_UpdateInventoryAnimation(void);
-
-void pascal SetMapSize(word width);
-void pascal ParseLevelFlags(
-  byte flags,
-  byte secondaryBackdrop,
-  byte unused1,
-  byte unused2);
-void pascal DecompressRLE(byte far* src, byte far* dest);
-
-void ShowTextScreen(const char far* filename);
-void ShowVGAScreen(const char far* filename);
-
-bool PlayVideo(char far* filename, word videoType, int16_t numRepetitions);
-
-
-// Functions from unit2.c
-int16_t DN2_abs(int16_t value);
-
-void pascal PlaySound(int16_t id);
-
-void pascal DrawSprite(word id, word frame, word x, word y);
-void pascal DrawFontSprite(word charIndex, word x, word y, word plane);
-
-void DrawNewHighScoreEntryBackground(void);
-
-void DrawNewsReporterTalkAnim(void);
-bool pascal QueryOrToggleOption(bool toggle, byte optionId);
-
-word Map_GetTile(word x, word y);
-void Map_SetTile(word tileIndex, word x, word y);
-void pascal
-  Map_MoveSection(word left, word top, word right, word bottom, word distance);
-
-void Quit(const char* quitMessage);
-
-
-// Variables from unit1.c
-extern byte gfxCurrentPalette[16 * 3];
-extern bool jsButtonsSwapped;
-extern byte far mapExtraData[8304];
